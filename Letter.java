e4753fad2a9f87f4b264526d6cbdc8ee26a99270
denizyagiz <45318039+denizyagiz@users.noreply.github.com>
2020-10-14 16:13:47 -0400

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,17 @@
+// Class: CS 5000
+// Term: Spring 2020
+// Name: Deniz Yagiz
+// Instructor: Dr. Haddad
+// Assignment: 1
+// IDE Name: Jgrasp
+public class Letter {
+//Main method
+   public static void main(String[] args) {
+//Display results
+      System.out.println("BBBB    EEEEE  X   X");
+      System.out.println("B   B   E       X X");
+      System.out.println("BBBB    EEEE     X");
+      System.out.println("B   B   E       X X");
+      System.out.println("BBBB    EEEEE  X   X");
+   }
+}
